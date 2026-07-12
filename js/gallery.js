c06524a7fd4131cbf58c414fdfcd0ed7cadb2ed0
MG207HUNG <karikahami@gmail.com>
2026-07-12 22:38:54 +0700

    final project

--- js/gallery.js
+++ js/gallery.js
@@ -1,28 +1,31 @@
 /*Name this external file gallery.js*/
 
-function upDate(previewPic){
- /* In this function you should 
-    1) change the url for the background image of the div with the id = "image" 
-    to the source file of the preview image
-    
-    2) Change the text  of the div with the id = "image" 
-    to the alt text of the preview image 
-    */
-    console.log("upDate triggered");
+function upDate(previewPic) {
+
+	console.log("upDate triggered");
 	console.log("alt:", previewPic.alt, "| src:", previewPic.src);
-    document.getElementById("image").innerHTML = previewPic.alt;
-    document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
-	}
+	document.getElementById("image").innerHTML = previewPic.alt;
+	document.getElementById("image").style.backgroundImage = "url('" + previewPic.src + "')";
+}
 
-	function unDo(){
-     /* In this function you should 
-    1) Update the url for the background image of the div with the id = "image" 
-    back to the orginal-image.  You can use the css code to see what that original URL was
-    
-    2) Change the text  of the div with the id = "image" 
-    back to the original text.  You can use the html code to see what that original text was
-    */
-		document.getElementById("image").style.backgroundImage = "url('')";
-		document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+function unDo() {
 
+	console.log("unDo triggered");
+	document.getElementById("image").style.backgroundImage = "url('')";
+	document.getElementById("image").innerHTML = "Hover over an image below to display here.";
+}
+
+function addTabIndex() {
+
+
+	console.log("addTabIndex triggered - page has loaded");
+
+
+	var images = document.getElementsByClassName("preview");
+
+
+	for (var i = 0; i < images.length; i++) {
+		images[i].setAttribute("tabindex", i + 1);
+		console.log("tabindex set for image " + (i + 1) + ":", images[i].alt);
 	}
+}
